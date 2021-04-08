b9b9c5ed6f6ce726e0186f5dfd4aa7ed99fc3ae4
Jon Rhea <indianarhea@gmail.com>
2021-04-08 11:58:52 -0400

    End of Day 4/8

--- Assets/_Scripts/FollowCam.cs
+++ Assets/_Scripts/FollowCam.cs
@@ -24,9 +24,31 @@ public class FollowCam : MonoBehaviour
 
 	void FixedUpdate () {
 		if (POI == null) return;  // make sure we know the POI
+		
+		Vector3 destination;
+		// If there is no poi, return to P:[ 0, 0, 0 ]
+		if (POI ==null ) {
+		destination =Vector3.zero;
+		}else {
+		// Get the position of the poi
+		destination = POI.transform.position;
+		// If poi is a Projectile, check to see if it's at rest
+		if (POI.tag == "Projectile" ) {
+		// if it is sleeping (that is, not moving)
+		if ( POI.GetComponent<Rigidbody>().IsSleeping() ) {
+		// return to default view
+		POI =null ;
+		// in the next update
+		return ;
+		}
+		}
+		}
+		destination.x = Mathf.Max( minXY.x, destination.x );
+
+
 
 		// Get the position of the POI
-		Vector3 destination = POI.transform.position;
+		/*Vector3 destination = POI.transform.position;
 		destination.x = Mathf.Max (minXY.x, destination.x);
 		destination.x = Mathf.Max (minXY.y, destination.y);
 
@@ -37,7 +59,7 @@ public class FollowCam : MonoBehaviour
 		// sets the camera to the destination
 		transform.position = destination;
 
-		Camera.main.orthographicSize = destination.y + 10;
+		Camera.main.orthographicSize = destination.y + 10;*/
 	
 	}
 
